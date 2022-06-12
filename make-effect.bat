8d441c105bcac9d81815dad56c3a1175f88c5ac9
joonas <joonas1000@gmail.com>
2022-06-12 20:56:29 +0300

    Change dllwrap to g++ -shared

--- make-effect.bat
+++ make-effect.bat
@@ -1,9 +1,9 @@
 : "-DFST_DEBUG_OUTPUT=""D:\\code\\c\\fst\\src\\log.txt"""
-g++ -c StaticCompressorPlugin.cpp ../fst/src/FstAudioEffect.cpp -I"../fst/src" -w
+g++ -static -c StaticCompressorPlugin.cpp ../fst/src/FstAudioEffect.cpp -I"../fst/src" -w
 
-dllwrap  --output-def libPlugin.def  --driver-name c++ ^
+g++ -shared ^
 StaticCompressorPlugin.o FstAudioEffect.o ^
--L. --add-stdcall-alias -lole32 -lkernel32 -lgdi32 -lgdiplus -luuid -luser32 -lshell32 -mwindows --no-export-all-symbols --def Plugin.def ^
+-L. -lole32 -lkernel32 -lgdi32 -lgdiplus -luuid -luser32 -lshell32 --def Plugin.def ^
 -o StaticCompressor.dll -Ofast
 
 : Clean up
